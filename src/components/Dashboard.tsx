9c8ba94b6b662f1fc1d586ca2b2dc5bec9ace141
hamdi <hamdi.pirillo4@gmail.com>
2024-09-17 14:57:39 +0100

    defined the 'boardColors' variable outside of the component so it is only defined once.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
 
-import Userboard from "./UserBoard"
+import Userboard from "./Userboard"
 import Board from "./Board"
 
 import { Data } from "../types/Data"
+
+const boardColors = [
+    'bg-primary-light-red-work',
+    'bg-primary-soft-blue-play',
+    'bg-primary-light-red-study',
+    'bg-primary-lime-green-exercise',
+    'bg-primary-voilet-social',
+    'bg-primary-soft-orange-self-care'
+]
 
 export default function Dashboard({
     data
@@ -11,15 +20,6 @@ export default function Dashboard({
     data: Data[]
 }) {
     const [currentlyActive, setCurrentlyActive] = React.useState('Weekly')
-
-    const boardColors = [
-        'bg-primary-light-red-work',
-        'bg-primary-soft-blue-play',
-        'bg-primary-light-red-study',
-        'bg-primary-lime-green-exercise',
-        'bg-primary-voilet-social',
-        'bg-primary-soft-orange-self-care'
-    ]
 
     return (
         <div className="flex gap-5">
